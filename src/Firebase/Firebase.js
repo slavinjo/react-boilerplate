6a43097f73b4884f71b07f2356cf5134d68174cd
Slaven Robic <slaven.robic@gmail.com>
2019-03-08 13:44:24 +0100

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,8 +1,12 @@
 import app from 'firebase/app';
 
 const config = {
- 
-    
+    apiKey: 'AIzaSyDVHJl5LsT_XVB6pTC8_zq3t1WHJcE9wiA',
+    authDomain: 'react-shop-project.firebaseapp.com',
+    databaseURL: 'https://react-shop-project.firebaseio.com',
+    projectId: 'react-shop-project',
+    storageBucket: 'react-shop-project.appspot.com',
+    messagingSenderId: '164676623194',
   };
 
 
