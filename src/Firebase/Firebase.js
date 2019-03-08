d1592e511bc3329748703217882698edca4793bb
Slaven Robic <slaven.robic@gmail.com>
2019-03-08 12:44:31 +0100

    Revert "Update Firebase.js"
    This reverts commit 961413740a04605030770209008fecfecbc8a5fc.

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,12 +1,7 @@
 import app from 'firebase/app';
 
 const config = {
-    apiKey: 'AIzaSyDVHJl5LsT_XVB6pTC8_zq3t1WHJcE9wiA',
-    authDomain: 'react-shop-project.firebaseapp.com',
-    databaseURL: 'https://react-shop-project.firebaseio.com',
-    projectId: 'react-shop-project',
-    storageBucket: 'react-shop-project.appspot.com',
-    messagingSenderId: '164676623194',
+
   };
 
 
